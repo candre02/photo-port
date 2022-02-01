1b3d9d73b72fe7640b9ed35ee8c679ab86a067f2
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-01-31 18:54:47 -0600

    started tests on about and nav folders

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,39 @@
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Nav />);
+  });
+})
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+    // return the element containg the emoji
+  const { getByLabelText } = render(<Nav />);
+  // 
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})  
+
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
+    // Arrange
+    const { getByTestId } = render(<Nav />);
+    // Assert
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+})
