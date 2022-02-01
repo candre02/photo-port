b1a39122f53a63ef672c41d16ca4e258bc1439dc
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-01 00:41:48 -0600

    created photolist, gallery, added content, tested files

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -10,30 +10,28 @@ describe('Nav component', () => {
   it('renders', () => {
     render(<Nav />);
   });
-})
 
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    
+
     expect(asFragment()).toMatchSnapshot();
   });
-
+})
 
 describe('emoji is visible', () => {
   it('inserts emoji into the h2', () => {
-    // return the element containg the emoji
-  const { getByLabelText } = render(<Nav />);
-  // 
-  expect(getByLabelText('camera')).toHaveTextContent('📸');
+    const { getByLabelText } = render(<Nav />);
+
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-})  
+})
 
 describe('links are visible', () => {
   it('inserts text into the links', () => {
-    // Arrange
     const { getByTestId } = render(<Nav />);
-    // Assert
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
+
 })
